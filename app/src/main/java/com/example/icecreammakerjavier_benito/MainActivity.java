2883cb9b7f9f670bc2660f94aa5ba37ba88e3011
Javier Benito Santoni <javierbenitosantoni@icloud.com>
2019-11-04 11:06:09 +0100

    First

--- app/src/main/java/com/example/icecreammakerjavier_benito/MainActivity.java
+++ app/src/main/java/com/example/icecreammakerjavier_benito/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.icecreammakerjavier_benito;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
