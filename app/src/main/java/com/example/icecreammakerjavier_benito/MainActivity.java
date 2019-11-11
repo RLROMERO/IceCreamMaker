109d8b85070f2dd44f2940f3abab8b44105256f7
Javier <javierbenitosantoni@icloud.com>
2019-11-11 19:07:15 +0100

    Arreglado

--- app/src/main/java/com/example/icecreammakerjavier_benito/MainActivity.java
+++ app/src/main/java/com/example/icecreammakerjavier_benito/MainActivity.java
@@ -4,27 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    int preciovar = 0;
-    int cantidadvar = 0;
     TextView cantidadnum;
     TextView precionum;
+    Button mas;
+    Button menos;
+    private int preciovar = 0;
+    private int cantidadvar = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         cantidadnum = findViewById(R.id.cantidadnum);
         precionum = findViewById(R.id.precionum);
-    }
-    public void onClickButtonMas(View view) {
-        incrementar(cantidadvar);
-    }
-    public void onClickButtonMenos(View view) {
-        decrementar(cantidadvar);
+        mas = (Button)findViewById(R.id.mas);
+        menos = (Button)findViewById(R.id.menos);
+
+        mas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cantidadvar++;
+                cantidadnum.setText(String.valueOf(cantidadvar));
+                precio();
+            }});
+        menos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cantidadvar--;
+                cantidadnum.setText(String.valueOf(cantidadvar));
+                precio();
+            }});
+
     }
     public void onClickButtonOrder(View view) {
         displayAndCalculatePriceInformation("3", "6");
@@ -34,13 +49,9 @@ public class MainActivity extends AppCompatActivity {
         cantidadnum.setText(cantidad);
         precionum.setText(precio);
     }
-    public void incrementar (Integer cantidadvar) {
-        cantidadvar++;
-        cantidadnum.setText(cantidadvar);
-    }
-    public void decrementar (Integer cantidadvar) {
-        cantidadvar--;
-        cantidadnum.setText(cantidadvar);
+    public void precio () {
+        preciovar = cantidadvar * 3;
+        precionum.setText(preciovar+"");
     }
 }
 
